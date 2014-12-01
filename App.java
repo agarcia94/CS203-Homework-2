cab5e6f3900e3abcf4a000172ef3c39ec7e191b7
lakerfan94 <mattgarcia94@sbcglobal.net>
2014-11-30 17:23:23 -0800

    Update App.java

--- App.java
+++ App.java
@@ -1,10 +1,7 @@
-import java.io.BufferedReader;
+package hw3;
+
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class App {
 
@@ -12,60 +9,45 @@ public class App {
         final String roadFilename = args[0];
         final String imageFilename = args[1];
         final String reportFilename = args[2];
-        final int numCellTower = Integer.parseInt(args[3]);
-        final double sigma = Double.parseDouble(args[4]);
+        final double x1= Double.parseDouble(args[3]);
+        final double y1= Double.parseDouble(args[4]);
+        final double x2=Double.parseDouble(args[5]);
+        final double y2=Double.parseDouble(args[6]);
+      
 
         final boolean isOSM = true;
-
-        RoadNetwork roadNetwork = 
-                new RoadNetwork(new java.io.File(roadFilename), isOSM);
-        roadNetwork.draw();
-        
-        for(Road road: roadNetwork.roads)
-        	road.setPointAmount();
-        
-        
-        Road mostPoints= roadNetwork.hasMostPoints();  //road with the most post points in the road network
-        
-
-        /*
-         * Get a list of all of the cellular network configurations
-         */
-
-        
-        ArrayList<CellNetwork> cellNetworkConfigurations =
-                mostPoints.getAllCellConfigurations(numCellTower, sigma);
-        
-        for(CellNetwork networks: cellNetworkConfigurations)
-        	networks.setAmountOfCoveredPoints(mostPoints);      //fill in the counters for the numbers of covered points for each
-        														//cell network
-        
-        
-        CellNetwork bestCellConfiguration = null;
-        
-        int bestScore = 0;
       
-        for (CellNetwork cn : cellNetworkConfigurations) {
-            // do test on cn to see if cn's score is better than bestScore
-            // if it is update bestCellConfiguration = cn
-        	if(cn.pointsCovered() > bestScore){
-        		bestScore = cn.coveredPoints;
-        		bestCellConfiguration = cn;
-        	}
-        		
-        }
-        
-        bestCellConfiguration.draw();
-        
-
-        /*
-         * Everything is done
-         * 
-         */
+        RoadNetwork roadNetwork;
+		try {
+			File file = new File(roadFilename);
+			roadNetwork = new RoadNetwork(file, isOSM);
+			roadNetwork.draw();
+	        
+			Point startPoint= roadNetwork.findNearestPoint(x1, y1);  //find the nearest point to the point defined by x1,y1
+			Point endPoint= roadNetwork.findNearestPoint(x2, y2); // find the nearest point to the points defined by x2,y2
+			roadNetwork.bfs(startPoint, endPoint);  //try to find the route from start to end, using breadth-first search
+			
+			
+			
+			
+			/*
+			
+			try {
+				File file2 = new File(reportFilename);
+				Report re = new Report(file2);
+				re.write(mostPoints, bestCellConfiguration, startTime, finishTime);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			*/
+	        
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
         StdDraw.save(imageFilename);
-        Report report = 
-                new Report(new java.io.File(reportFilename));
-        report.write();
+			
     }
 }
