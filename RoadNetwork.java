5b03676d9cdb74a6f01d8f3e84edad326b141ef6
lakerfan94 <mattgarcia94@sbcglobal.net>
2014-11-30 17:25:53 -0800

    Update RoadNetwork.java

--- RoadNetwork.java
+++ RoadNetwork.java
@@ -1,18 +1,27 @@
+package hw3;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 
 public class RoadNetwork {
 
-    java.util.ArrayList<Road> roads =  new java.util.ArrayList<Road>() ;
+    java.util.ArrayList<Road> roads =  new java.util.ArrayList<Road>();
+    java.util.ArrayList<Segment> segments =  new java.util.ArrayList<Segment>();
     HashMap<Long, Point> allPoints = new HashMap<Long, Point>();
-    ArrayList<CellNetwork> networks=new ArrayList<CellNetwork>();
+    java.util.ArrayList<Long> roadRef = new java.util.ArrayList<Long>();
+    java.util.ArrayList<Point> tempPoints = new java.util.ArrayList<Point>();
+    HashMap<Long, ArrayList<Segment>> pointNeighbors= new HashMap<Long, ArrayList<Segment>>();
+    
     private Scanner scanner;
     
     /*
@@ -45,36 +54,144 @@ public class RoadNetwork {
                 cn.add(tower);
             }
             cellNetworks.add(cn);
-            networks.add(cn);
         }
         
         return cellNetworks;
     }
-
-    private void readTextFormat(File file) {
-        Road road = new Road();
-        java.util.ArrayList<Point> tempPoints = 
-                new java.util.ArrayList<Point>();
-        try {
-            scanner = new Scanner( file );
-            while ( scanner.hasNext() ) {
-                String line = scanner.nextLine();
-                Point p = new Point( line );
-                Boundaries.update( p );
-                tempPoints.add( p );
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        road.build( tempPoints );
-        roads.add(road);
+    
+    public void searchNeighbors(Point p){  //fill in the arraylist of points that are neighbors of the given point p
+    	ArrayList<Segment> neighbors=new ArrayList<Segment>();
+//    	ArrayList<Point> neighborPoints = new ArrayList<Point>();
+    	
+    	for(Road road : roads){
+    		for(Segment s : road.segments){
+    			if(s.p1.equals(p) || s.p2.equals(p))
+    				neighbors.add(s);
+    		}
+    	}
+    
+//    	pointNeighbors.put(p.id, neighbors);
+//    	p.setListOfEdges(neighbors);
+    	
+    	for(Segment s : neighbors){
+    		if(s.p1.equals(p))
+    			p.setNeighborPoints(s.p2);
+    		
+    		if(s.p2.equals(p))
+    			p.setNeighborPoints(s.p1);
+    			
+    	}
+    	
+    }
+    
+    public HashMap<Long, ArrayList<Segment>> getPointNeighbors(){  //might use it, but ignore it for now 
+    	return pointNeighbors;
+    }
+    
+    public Point findNearestPoint(double lon, double lat){  //find the nearest point in the road network closest to the point 
+    	Point nearestPoint = null;							// given by a lon and lat
+    	double distance= Double.MAX_VALUE;
+    	ArrayList<Point> points = (ArrayList<Point>) allPoints.values();
+    	
+    	for(Point p : points){
+    		if(p.distance(lon, lat) < distance){
+    			distance = p.distance(lon, lat);
+    			nearestPoint = p;
+    		}
+    	}
+    
+    	return nearestPoint;
+    }
+    
+    public void bfs(Point p1, Point p2){ //breadth-first search from p1 to p2
+    	p1.setIsVisted(true);
+    	this.searchNeighbors(p1);
+    	Queue<Point> queue= new LinkedList<Point>();
+    	queue.add(p1);
+    	
+    	while(!queue.isEmpty()){
+    		ArrayList<Point> neighborPoints= p1.getNeighborPoints();
+    		for(Point p : neighborPoints){
+    			if(p.isVisited() == false){
+    				p.setIsVisted(true);
+    				queue.add(p);
+    			}else
+    				queue.remove(p);
+    		}
+    	}
+    	
+  
     }
 
-    public RoadNetwork(File file) {
+    private void readTextFormat(File file) throws IOException {
+    	BufferedReader br = new BufferedReader(new FileReader(new File("text.txt")));
+    	String line = "";
+    	try {
+			while((line = br.readLine()) != null){
+				
+				String space = " ";
+				String[] components = line.split(space);
+				String[] newc = line.split(space);
+				
+				if(line.startsWith("boundaries:")){
+					double xmi = Double.valueOf(components[1]);
+					double ymi = Double.valueOf(components[2]);
+					double xma = Double.valueOf(components[3]);
+					double yma = Double.valueOf(components[4]);
+					Boundaries.xmax = xma;
+					Boundaries.ymax = yma;
+					Boundaries.xmin = xmi;
+					Boundaries.ymin = ymi;
+				}
+				
+				
+				if(line.startsWith("P")){
+				long id = Long.valueOf(components[1]);
+				double x = Double.valueOf(components[2]);
+				double y = Double.valueOf(components[3]);
+				Point p = new Point(id, x, y);
+				allPoints.put(id, p);
+				}
+				
+				if(line.startsWith("R")){
+					java.util.ArrayList<Long> pointsID = new java.util.ArrayList<Long>();
+					Queue<Long> q = new LinkedList<Long>();
+					for(int i = 1; i < components.length; i++){
+						q.add(Long.valueOf(components[i]));
+						pointsID.add(Long.valueOf(components[i]));
+						//System.out.println(q);
+						System.out.println(pointsID);
+					}
+					for(int i = 1; i < pointsID.size(); i++){
+						roadRef.add(pointsID.get(0));
+						Point p1 = allPoints.get(pointsID.get(i));
+						//Point p = allPoints.get(q.poll());
+						//System.out.println(p);
+						//tempPoints.add(p);
+						tempPoints.add(p1);
+						System.out.println(tempPoints);
+					}
+					Road r = new Road();
+					r.build(tempPoints);
+					System.out.println(r.segments);
+					roads.add(r);
+				}
+			}
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    		br.close();
+    }
+
+    public RoadNetwork(File file) throws IOException {
         readTextFormat(file);
     }
 
-    /*
+
     private void readOSMFormat(File file) {
         // extract boundaries
         try {
@@ -106,18 +223,19 @@ public class RoadNetwork {
         }
 
         // extract ways: implement your code below
+//        try {
+//        	scanner = new Scanner(file);
+//        }
     }
-    */
 
-    public RoadNetwork(File file, boolean isOSM) {
+    public RoadNetwork(File file, boolean isOSM) throws IOException {
         if (isOSM) {
-            OSM.extractBoundaries(file); 
+            OSM.extractBoundaries(file);
             allPoints= OSM.extractNodes(file);
             roads.addAll(OSM.extractWays(file, allPoints).values());
-            
-        } else 
+        } else {
             readTextFormat(file);
-        
+        }
     }
 
     public void draw() {
@@ -132,22 +250,37 @@ public class RoadNetwork {
         }
     }
     
-    public HashMap<Long, Integer> roadPointAmount(){  //contains a map of the roads in the network, each road with its own amount of points
-    	HashMap<Long, Integer> amount=new HashMap<Long, Integer>();
-    	for(Road r : roads)
-    		amount.put(r.id, r.getTotalPointAmount());
+    public int highestPointAmount(){  
+    	ArrayList<Integer> points=new ArrayList<Integer>();
+    	for(Road road : roads)
+    		points.add(road.getTotalPointAmount());
     	
-    	return amount;
+    	int highest=0;
+    	for(int i : points){
+    		if( i > highest)
+    			highest = i;
+    	}
+    	
+    	return highest;
     }
     
     public Road hasMostPoints(){  //finds the road in the network that has the most points
-    	int mostPoints= Collections.max(roadPointAmount().values());
+    	int mostPoints= highestPointAmount();
+    	Road road=new Road();
     	for(Road r : roads){
     		if(r.getTotalPointAmount() == mostPoints)
-    			return r;
+    			road=r;
     	}
     	
-    	return null;
+    	return road;
+    }
+    
+    public int getPointAmount(){
+    	int sum=0;
+    	for (Road road: roads){
+    		sum += road.getTotalPointAmount();
+    	}
+    	return sum;
     }
    
 //    public ArrayList<CellNetwork> getNetworks(){
